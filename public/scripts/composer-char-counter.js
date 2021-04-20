b12929f62e64cf00c0a12d7b8cd82f9fe4cd3819
Mohamed Hassan <mhass405@gmail.com>
2021-04-20 20:53:23 +0000

    Implement character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  let charLimit = 140; 
+  const textArea = $('.new-tweet form textarea');
+  console.log(textArea);
+
+  textArea.on('input', function() {
+    // your code here
+    const charAvailable = 140 - $(this).val().length;
+
+    let tweetCount = $(this).siblings('div').children('.counter');
+
+    tweetCount[0].innerText = charAvailable
+    if (charAvailable < 0) {
+      tweetCount.css('color', 'red')
+    } else {
+      tweetCount.css('color', 'inherit')
+    }
+  });
+});
