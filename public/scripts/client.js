574b4ad6778f60e965768d64bbd55a5ac9250707
Mohamed Hassan <mhass405@gmail.com>
2021-04-22 18:40:36 +0000

    fix time ago and character limit issue

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,33 +2,9 @@
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- // $("time.timeago").timeago();
+ // 
  */
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -39,9 +15,11 @@ const createTweetElement = function (tweetObj) {
   const userName = tweetObj.user.name
   const avatar = tweetObj.user.avatars
   const handle = tweetObj.user.handle
-  const text = tweetObj.content.text
+  let text = tweetObj.content.text
+  const tweetLength = text.length;
   const safeHTML = escape(text)
-  
+  const time = Date.now()
+
   const tweet = $(` <article>
   <header>
   <div><img class="avatar" src= "${avatar}"></img> ${userName}</div>
@@ -49,7 +27,7 @@ const createTweetElement = function (tweetObj) {
   </header>
   <p class="twitter-display">${safeHTML}</p>
   <footer>
-  <time class="timeago" datetime="new Date">10 days ago</time>
+  <time class="timeago" datetime="${time}"></time>
   <div>
   <i class="fas fa-flag"></i>
   <i class="fas fa-retweet"></i>
@@ -61,22 +39,23 @@ const createTweetElement = function (tweetObj) {
 }
 
 const renderTweets = function(tweets) {
-
   for (const tweet of tweets) {
+    
     $('.tweet-display').prepend(createTweetElement(tweet));
   }
+  timeago.render(document.querySelectorAll('.timeago'));
 }
 
 const loadTweets = function () {
-
+  
   $.ajax('/tweets', {dataType: "json", method: "GET"})
   .then(function (data) {
     renderTweets(data);
   })
+
 }
 
 const getNewTweets = function () {
-
   $.ajax('/tweets', {dataType: "json", method: "GET"})
   .then(function (data) {
     renderTweets([data[data.length - 1]]);
@@ -84,28 +63,28 @@ const getNewTweets = function () {
 }
 
 $(document).ready(function() {
-
+  
   loadTweets();
-  $("#tweet-form").on("submit", function (event) {
 
+  $("#tweet-form").on("submit", function (event) {
+    
     event.preventDefault();
-    const tweetLength = event.target[0].value.length;
-    
-    if (!tweetLength) {
-      alert("Your tweet is either empty or null")
-    }
-    
-    if ( tweetLength > 140) {
-      alert('Your tweet exceeded the maximum allowed characters')
-    }
-    
     const serialData = $(this).serialize()
+    const tweetText = $('#tweet-text')[0].value
+
+    $(".error").hide();
+
+    if (tweetText.length > 140) {
+      $( ".error" ).slideDown( "slow", function() {
+        $(".error").show();
+      });
+      return;
+    }
+
     $.post("/tweets", serialData)
     .then (() => {
-                                         
       getNewTweets();
     })
 
   })
 });
-// timeago.render(document.querySelectorAll('.timeago'));
