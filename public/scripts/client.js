277e5285ac27f21fc2d9dbc1fddeb805b83550b5
Mohamed Hassan <mhass405@gmail.com>
2021-04-25 22:42:07 +0000

    Move functions to helper file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,87 +1,3 @@
-/*----------------- Client-side JS -----------------*/
-
-/*------------ Prevent script injection ------------*/
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
-
-/*------------------ Create tweet ------------------*/
-const createTweetElement = function (tweetObj) {
-  const userName = tweetObj.user.name
-  const avatar = tweetObj.user.avatars
-  const handle = tweetObj.user.handle
-  const text = tweetObj.content.text
-  const safeHTML = escape(text)
-  const time = Date.now()
-
-  const tweet = $(`
-  <article>
-    <header>
-      <div class="user">
-        <img class="avatar" src= "${avatar}"></img> 
-        <p class="user-name">${userName}</p>
-      </div>
-      <i class="avatar-handle">${handle}</i>
-    </header>
-    <p class="twitter-display">${safeHTML}</p>
-    <footer>
-      <time class="timeago" datetime="${time}"></time>
-      <div>
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </div>
-    </footer> 
-  </article>
-  `); 
-  return tweet;
-};
-
-/*-------- Loop  through database to render tweets --------*/
-const renderTweets = function(tweets) {
-
-  for (const tweet of tweets) {
-    $('.tweet-display').prepend(createTweetElement(tweet));
-  }
-
-  timeago.render(document.querySelectorAll('.timeago'));
-};
-
-/*--------------- Render tweets using AJAX ---------------*/
-const loadTweets = function () {
-  
-  $.ajax('/tweets', {dataType: "json", method: "GET"})
-  .then(function (data) {
-    renderTweets(data);
-  })
-};
-/*------------- Render new tweet using AJAX -------------*/
-const getNewTweets = function () {
-
-  $.ajax('/tweets', {dataType: "json", method: "GET"})
-  .then(function (data) {
-    renderTweets([data[data.length - 1]]);
-  })
-};
-
-/*-------- Show button when after scrolling 400 --------*/
-const toTopOfPage = function () {
-  $(window).on("scroll", function () {
-
-    if ($(window).scrollTop() >= 400){
-      $("#top-page").show();
-  
-      $("#top-page").on("click", function () {
-        $(window).scrollTop(0);
-      })
-    } else {
-      $("#top-page").hide();
-    }
-  })
-}
-
 $(document).ready(function() {
   
   loadTweets();
@@ -92,8 +8,8 @@ $(document).ready(function() {
     event.preventDefault();
 
     const serialData = $(this).serialize()
-    const tweetText = $('#tweet-text')[0].value
 
+    let tweetText = $('#tweet-text').val()
     $(".error-max").hide();
     $(".error-empty").hide();
 
---
+++ public/scripts/helpers.js
@@ -0,0 +1,84 @@
+/*----------------- Client-side JS -----------------*/
+
+/*------------ Prevent script injection ------------*/
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+/*------------------ Create tweet ------------------*/
+const createTweetElement = function (tweetObj) {
+  const userName = tweetObj.user.name
+  const avatar = tweetObj.user.avatars
+  const handle = tweetObj.user.handle
+  const text = tweetObj.content.text
+  const safeHTML = escape(text)
+  const time = timeago.format(tweetObj.created_at);
+  
+  const tweet = $(`
+  <article>
+    <header>
+      <div class="user">
+        <img class="avatar" src= "${avatar}"></img> 
+        <p class="user-name">${userName}</p>
+      </div>
+      <i class="avatar-handle">${handle}</i>
+    </header>
+    <p class="twitter-display">${safeHTML}</p>
+    <footer>
+      <time class="time">${time}</time>
+      <div>
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer> 
+  </article>
+  `); 
+  return tweet;
+};
+
+/*-------- Loop  through database to render tweets --------*/
+const renderTweets = function(tweets) {
+
+  for (const tweet of tweets) {
+    $('.tweet-display').prepend(createTweetElement(tweet));
+  }
+
+  timeago.render(document.querySelectorAll('.timeago'));
+};
+
+/*--------------- Render tweets using AJAX ---------------*/
+const loadTweets = function () {
+  
+  $.ajax('/tweets', {dataType: "json", method: "GET"})
+  .then(function (data) {
+    renderTweets(data);
+  })
+};
+/*------------- Render new tweet using AJAX -------------*/
+const getNewTweets = function () {
+
+  $.ajax('/tweets', {dataType: "json", method: "GET"})
+  .then(function (data) {
+    renderTweets([data[data.length - 1]]);
+  })
+};
+
+/*-------- Show button when after scrolling 400 --------*/
+const toTopOfPage = function () {
+  $(window).on("scroll", function () {
+
+    if ($(window).scrollTop() >= 400){
+      $("#top-page").show();
+  
+      $("#top-page").on("click", function () {
+        $(window).scrollTop(0);
+      })
+    } else {
+      $("#top-page").hide();
+    }
+  })
+}
+
